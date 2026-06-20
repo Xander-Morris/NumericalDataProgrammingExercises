447a74ff92ecdfd2236fca3c9a85c9caf196bdbe
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-06-19 19:39:43 -0700

    work

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# The following lines adjust the granularity of reporting.
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
+training_df = pd.read_csv(filepath_or_buffer="https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv")
+
+training_df.describe()
