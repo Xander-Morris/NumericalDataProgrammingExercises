955aaa00ef7769f09cae7aa32e457635180af278
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-06-19 19:40:02 -0700

    work

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ pd.options.display.float_format = "{:.1f}".format
 
 training_df = pd.read_csv(filepath_or_buffer="https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv")
 
-training_df.describe()
+print(training_df.describe())
